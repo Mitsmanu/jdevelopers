5f99c3eff7ac790706272dc623ee0af173ed9e40
Manuel Herrera <manuelh0107g@gmail.com>
2020-05-27 14:31:21 -0500

    “prueba”

---
+++ clock.js
@@ -0,0 +1,5 @@
+ <script type="text/javascript">$(function ()
+{
+$('#clock').epiclock({mode: 'countdown', target: '2021 07 21 10:30:00'});
+});
+</script>
---
+++ main.js
@@ -0,0 +1,3 @@
+function saluda() {
+            alert('Hola')
+}
